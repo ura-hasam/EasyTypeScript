3425018033fe25a0e02021ac11224d63b93d3b17
ura-hasam <s.yamane0601@gmail.com>
2024-07-12 10:23:23 +0900

    ４章章末問題の解答

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const x=10;
-const y=2;
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x%y);
+let x=10;
+let y=2;
+x +=1;
+y -=2;
+console.log(x);
+console.log(y);
+
+
+let age1=25;
+let age2=30;
+console.log(age1<age2);
+console.log(age1>age2);
+console.log(age1<=age2);
+console.log(age1>=age2);
+console.log(age1===age2);
+console.log(age1!==age2);
+
+const msg1:string="Hello";
+const msg2:string="World";
+const greeting:string=msg1+" "+msg2;
+console.log(greeting);
