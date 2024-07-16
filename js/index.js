3a2978be590e26d5b1d70a4052226dbe32ef39ee
ura-hasam <s.yamane0601@gmail.com>
2024-07-16 21:35:50 +0900

    三項演算子のネストの学習

--- js/index.js
+++ js/index.js
@@ -11,3 +11,7 @@ console.log(msg);
 //三項演算子の例
 msg = x > 10 ? "xは10より大きい" : "xは10以下です";
 console.log(msg);
+//三項演算子のネスト
+const grade = 75;
+const result = grade >= 90 ? "優秀" : grade >= 70 ? "良好" : "普通";
+console.log(result);
