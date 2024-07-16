03f0a563748b4c7d7a656f1be179b49c9328000c
ura-hasam <s.yamane0601@gmail.com>
2024-07-16 23:43:43 +0900

    ５章の章末復習問題の解答

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,66 @@
-const mon:number=11;
-let days:number=0;
-
-switch(mon){
-    case 1:
-        days=31;
-        break;
-    case 2:
-        days=28;
-        break;
-    case 3:
-        days=31;
-        break;
-    case 4:
-        days=30;
-        break;
-    case 5:
-        days=31;
-        break;
-    case 6:
-        days=30;
-        break;
-    case 7:
-        days=31;
-        break;
-    case 8:
-        days=31;
-        break;
-    case 9:
-        days=30;
-        break;
-    case 10:
-        days=31;
-        break;
-    case 11:
-        days=30;
-        break;
-    case 12:
-        days=31;
-        break;
-    default:
-        days=0;
-        break;
-}
-console.log(`${mon}月は${days}日あります`)
-//同一の日数を持つcase節をまとめる事が出来る。
-//このcase節をまとめる書き方をフォールスルーと呼ぶ
-const month:number=11;
-let day:number=0;
-
-switch(month){
-    case 1:case 3:case 5: case 7: case 8 :case 10:case 12:
-        day=31;
-        break;
-    case 2:
-        day=28;
-        break;
-    case 4:case 6:case 9:case 11:
-        day=30;
-        break;
-    default:
-        day=0;
-        break; 
-}
-console.log(`${month}月は${day}日です`);
+//問題5-1
+    console.log("問題5-1");
+        let num:number=3;
+        if(num>=10){
+            console.log("OK");
+        }else{
+            console.log("NG");
+        }
+        console.log("---------");
+        num>=10 ? console.log("OK") : console.log("NG");
+        console.log(num>=10 ? "OK":"NG");//答え
+    console.log("============");
+//問題5-2
+    console.log("問題5-2");
+        const color:string="orenge"
+        if(color==="red"){
+            console.log("赤");
+        }else if(color==="blue"){
+            console.log("青");
+        }else{
+            console.log("その他");
+        }
+        console.log("---------");
+        switch(color){
+            case "red":
+                console.log("赤");
+                break;
+            case "blue":
+                console.log("青");
+                break;
+            default:
+                console.log("その他");
+                break;
+        }
+    console.log("============");
+//問題5-3
+    console.log("問題5-3");
+        const score:number=75;
+        if(score>=80){
+            console.log("優");
+        }else if(score>=60){
+            console.log("良")
+        }else if(score>=40){
+            console.log("可");
+        }else{
+            console.log("不可");
+        }
+    console.log("============");
+//問題5-4
+    console.log("問題5-4");
+        const animal:string="cat";
+        switch(animal){
+            case "dog":
+                console.log("犬");
+                break;
+            case "cat":
+                console.log("猫");
+                break;
+            case "bird":
+                console.log("鳥");
+                break;
+            default:
+                console.log("該当なし");
+                break;
+        }
+    console.log("============");
