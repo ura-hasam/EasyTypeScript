c335f6d4dd71f0846ae53142cee61c46e0cbd986
ura-hasam <s.yamane0601@gmail.com>
2024-07-16 23:08:36 +0900

    switch文の学習

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,65 @@
-const x=5;
-let msg:string="";
+const mon:number=11;
+let days:number=0;
 
-if (x>10){
-    msg="xは10より大きい";
+switch(mon){
+    case 1:
+        days=31;
+        break;
+    case 2:
+        days=28;
+        break;
+    case 3:
+        days=31;
+        break;
+    case 4:
+        days=30;
+        break;
+    case 5:
+        days=31;
+        break;
+    case 6:
+        days=30;
+        break;
+    case 7:
+        days=31;
+        break;
+    case 8:
+        days=31;
+        break;
+    case 9:
+        days=30;
+        break;
+    case 10:
+        days=31;
+        break;
+    case 11:
+        days=30;
+        break;
+    case 12:
+        days=31;
+        break;
+    default:
+        days=0;
+        break;
 }
-else{
-    msg="xは10以下です";
+console.log(`${mon}月は${days}日あります`)
+//同一の日数を持つcase節をまとめる事が出来る。
+//このcase節をまとめる書き方をフォールスルーと呼ぶ
+const month:number=11;
+let day:number=0;
+
+switch(month){
+    case 1:case 3:case 5: case 7: case 8 :case 10:case 12:
+        day=31;
+        break;
+    case 2:
+        day=28;
+        break;
+    case 4:case 6:case 9:case 11:
+        day=30;
+        break;
+    default:
+        day=0;
+        break; 
 }
-console.log(msg);
-
-//三項演算子の例
-msg = x>10 ? "xは10より大きい" : "xは10以下です";
-console.log(msg);
-
-//三項演算子のネスト
-const grade=75;
-const result = grade>=90 ? "優秀" : grade >=70 ? "良好" : "普通";
-console.log(result);
+console.log(`${month}月は${day}日です`);
