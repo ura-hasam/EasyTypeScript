3cf53bde15e69e027fe74489462950bce3bd9f9c
ura-hasam <s.yamane0601@gmail.com>
2024-07-19 19:59:23 +0900

    多重ループの学習

--- js/index.js
+++ js/index.js
@@ -1,23 +1,7 @@
 "use strict";
-const kokugo = [80, 65, 90, 75, 85];
-let sum = 0;
-for (const num of kokugo) {
-    sum += num;
-}
-const average = sum / 5;
-console.log(`５人の国語の合計点数は${sum}点で、平均点数は
-    ${average}点です`);
-console.log("============================");
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-    if (i === 2) {
-        break;
+const array = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+for (let i = 0; i <= 2; i++) {
+    for (let j = 0; j <= 3; j++) {
+        console.log(array[i][j]);
     }
-}
-console.log("============================");
-for (let a = 0; a < 5; a++) {
-    if (a === 2) {
-        continue;
-    }
-    console.log(a);
 }
