edca97efa20cb49eeae6bae07833c6acde6b7e67
ura-hasam <s.yamane0601@gmail.com>
2024-07-19 19:39:06 +0900

    break,continueの学習

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 "use strict";
 const kokugo = [80, 65, 90, 75, 85];
 let sum = 0;
-for (let i = 0; i < 5; i++) {
-    sum += kokugo[i];
+for (const num of kokugo) {
+    sum += num;
 }
 const average = sum / 5;
 console.log(`５人の国語の合計点数は${sum}点で、平均点数は
     ${average}点です`);
+console.log("============================");
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+    if (i === 2) {
+        break;
+    }
+}
+console.log("============================");
+for (let a = 0; a < 5; a++) {
+    if (a === 2) {
+        continue;
+    }
+    console.log(a);
+}
