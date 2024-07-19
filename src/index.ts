c45ec42bf797297b9edad6c88880cd316c4931d5
ura-hasam <s.yamane0601@gmail.com>
2024-07-19 18:47:15 +0900

    while,do-while構文の学習

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,14 @@
-//問題5-1
-    console.log("問題5-1");
-        let num:number=3;
-        if(num>=10){
-            console.log("OK");
-        }else{
-            console.log("NG");
-        }
-        console.log("---------");
-        num>=10 ? console.log("OK") : console.log("NG");
-        console.log(num>=10 ? "OK":"NG");//答え
-    console.log("============");
-//問題5-2
-    console.log("問題5-2");
-        const color:string="orenge"
-        if(color==="red"){
-            console.log("赤");
-        }else if(color==="blue"){
-            console.log("青");
-        }else{
-            console.log("その他");
-        }
-        console.log("---------");
-        switch(color){
-            case "red":
-                console.log("赤");
-                break;
-            case "blue":
-                console.log("青");
-                break;
-            default:
-                console.log("その他");
-                break;
-        }
-    console.log("============");
-//問題5-3
-    console.log("問題5-3");
-        const score:number=75;
-        if(score>=80){
-            console.log("優");
-        }else if(score>=60){
-            console.log("良")
-        }else if(score>=40){
-            console.log("可");
-        }else{
-            console.log("不可");
-        }
-    console.log("============");
-//問題5-4
-    console.log("問題5-4");
-        const animal:string="cat";
-        switch(animal){
-            case "dog":
-                console.log("犬");
-                break;
-            case "cat":
-                console.log("猫");
-                break;
-            case "bird":
-                console.log("鳥");
-                break;
-            default:
-                console.log("該当なし");
-                break;
-        }
-    console.log("============");
+let currentPage =1;
+const totalPages =10;
+
+while (currentPage<=totalPages){
+    console.log(`現在${currentPage}ページをよんでいます`);
+    currentPage++;
+}
+console.log("全ページを読み終えました。")
+console.log("===================================")
+do{
+    console.log(`現在${currentPage}ページを読みました`);
+    currentPage++;  
+}while(currentPage <=totalPages);
+console.log("完了");
