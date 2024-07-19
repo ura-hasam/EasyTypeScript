86a1b6d0a98cc281fc39c94361bed9b272827ad2
ura-hasam <s.yamane0601@gmail.com>
2024-07-19 19:03:26 +0900

    for文の学習

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-let currentPage =1;
-const totalPages =10;
+const kokugo=[80,65,90,75,85];
 
-while (currentPage<=totalPages){
-    console.log(`現在${currentPage}ページをよんでいます`);
-    currentPage++;
+let sum =0;
+for(let i=0;i<5;i++){
+    sum += kokugo[i];
 }
-console.log("全ページを読み終えました。")
-console.log("===================================")
-do{
-    console.log(`現在${currentPage}ページを読みました`);
-    currentPage++;  
-}while(currentPage <=totalPages);
-console.log("完了");
+
+const average=sum/5;
+
+console.log(`５人の国語の合計点数は${sum}点で、平均点数は
+    ${average}点です`);
