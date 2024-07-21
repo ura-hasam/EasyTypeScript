d1c39c2f314faee39294ee46fd4ed2b0d15441a4
ura-hasam <s.yamane0601@gmail.com>
2024-07-21 18:28:43 +0900

    関数の学習

--- js/index.js
+++ js/index.js
@@ -1,45 +1,9 @@
 "use strict";
-console.log("問題6-1");
-let start = 0;
-const end = 10;
-while (start <= end) {
-    console.log(start);
-    start++;
+function getTriArea() {
+    let area = 0;
+    let base = 4;
+    let height = 3;
+    area = base * height / 2;
+    console.log(`面積は${area}です`);
 }
-start = 1;
-console.log("==============");
-console.log("問題6-2");
-const finish = 5;
-do {
-    console.log(start);
-    start++;
-} while (start <= finish);
-console.log("==============");
-console.log("問題6-3");
-for (let i = 0; i <= 4; i++) {
-    console.log(i);
-}
-console.log("==============");
-console.log("問題6-4");
-const array = ["a", "b", "c", "d", "e"];
-let sum = 0;
-for (let alphabet of array) {
-    console.log(alphabet);
-}
-console.log("==============");
-console.log("問題6-5");
-for (let j = 1; j <= 10; j++) {
-    console.log(j);
-    if (j === 5) {
-        break;
-    }
-}
-console.log("==============");
-console.log("問題6-6");
-for (let k = 1; k <= 10; k++) {
-    if (k % 3 === 0) {
-        continue;
-    }
-    console.log(k);
-}
-console.log("==============");
+getTriArea();
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,9 @@
-console.log("問題6-1")
-let start =0;
-const end=10;
-    while(start<=end){
-        console.log(start);
-        start++;
-    }
-    start=1;
-console.log("==============")
-console.log("問題6-2")
-const finish=5
-    do{
-        console.log(start);
-        start++;
-    }while(start<=finish);
-console.log("==============")
-console.log("問題6-3")
-    for(let i=0;i<=4;i++){
-        console.log(i);
-    }
-console.log("==============")
-console.log("問題6-4")
-    const array=["a","b","c","d","e"];
-    let sum=0;
-    for(let alphabet of array){
-        console.log(alphabet);
-    }
-console.log("==============")
-console.log("問題6-5")
-    for(let j=1;j<=10;j++){
-        console.log(j);
-        if(j===5){
-            break;
-        }
-    }
-console.log("==============")
-console.log("問題6-6")
-    for(let k=1;k<=10;k++){
-        if(k%3===0){
-            continue;
-        }
-        console.log(k)
-    }
-console.log("==============")
+function getTriArea():void{
+    let area:number=0;
+    let base:number=4;
+    let height:number=3;
+
+    area=base*height/2;
+    console.log(`面積は${area}です`);
+}
+getTriArea();
