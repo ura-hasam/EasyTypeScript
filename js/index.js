b1691a40092fa9e0b7b792ce1ed343bcda51199b
ura-hasam <s.yamane0601@gmail.com>
2024-07-22 22:28:30 +0900

    可変長引数

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 "use strict";
-function greet(name, greeting = "Hello") {
-    console.log(`${greeting},${name}!`);
+function sum(...nums) {
+    let total = 0;
+    for (let num of nums) {
+        total += num;
+    }
+    console.log(`合計は${total}です`);
 }
-greet("HIRO");
-greet("HIRO", "こんにちは");
+sum(1, 2, 3, 4);
+sum(5, 10);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-function greet(name:string,greeting:string="Hello"):void{
-    console.log(`${greeting},${name}!`);
+function sum(...nums:number[]):void{
+    let total =0;
+    for(let num of nums){
+        total +=num;
+    }
+    console.log(`合計は${total}です`);
 }
-greet("HIRO");
-greet("HIRO","こんにちは");
+sum(1,2,3,4);
+sum(5,10);
