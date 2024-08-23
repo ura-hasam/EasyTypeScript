282b33dcc925bdaaf53686eb6ad92bb5a17a170f
ura-hasam <s.yamane0601@gmail.com>
2024-08-23 19:34:21 +0900

    ９章クラスの応用＿継承についての学習

--- js/index.js
+++ js/index.js
@@ -1,27 +1,41 @@
 "use strict";
 class House {
-    // openDoor(isOn:boolean){
-    //     if(isOn){
-    //         console.log("ドアを開けました");
-    //     }
-    // }//メソッドの定義
-    openDoor() {
-        console.log("ドアを開けました");
-    }
-    set color(variation) {
-        this.color = variation;
-    } //setterの定義
-    get color() {
-        return this.color;
-    } //getterの定義
     constructor() {
-        this.adress = "東京"; //住所
         this.color = "白";
         this.rooms = 1;
     }
+    activateSecurity(isOn) {
+        if (isOn) {
+            console.log("セキュリティを作動しました");
+        }
+        else {
+            console.log("セキュリティを停止しました");
+        }
+    }
 }
-//以下はアクセス不可
-// redHouse.rooms=2;
-// redHouse.countRooms();
-// redHouse.address="千葉県"
-// redHouse.getAddress();
+class GarageHouse extends House {
+    constructor() {
+        super(...arguments);
+        //車庫の有無を表すプロパティ(true:車庫あり/false:車庫なし)
+        this.hasGarage = false;
+    }
+    //車庫の開閉をするメソッドの定義(true:車庫を開ける/false:車庫を閉める)
+    openGarage(isOpen) {
+        if (this.hasGarage) { //車庫があるかどうか
+            if (isOpen) { //車庫を開ける場合
+                console.log("車庫を開けました");
+            }
+            else { //車庫を閉じる場合
+                console.log("車庫を閉じました");
+            }
+        }
+        else { //車庫が無い場合
+            console.log("車庫がありません");
+        }
+    }
+}
+const gh = new GarageHouse(); //インスタンスの作成
+gh.hasGarage = true; //車庫を「あり」に設定
+gh.openGarage(true); //車庫を空ける
+gh.openGarage(false); //車庫を閉める
+gh.activateSecurity(true); //セキュリティを作動させる
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
 class House{
-    adress:string="東京";//住所
-    rooms:number;
-    // openDoor(isOn:boolean){
-    //     if(isOn){
-    //         console.log("ドアを開けました");
-    //     }
-    // }//メソッドの定義
-    openDoor():void{
-        console.log("ドアを開けました")
-    }
-    
-    set color(variation:string){
-        this.color=variation;
-    }//setterの定義
-    get color():string{
-        return this.color;
-    }//getterの定義
-    constructor(){
-        this.color="白";
-        this.rooms=1;
+    public color:string="白";
+    public rooms:number=1;
+
+    public activateSecurity(isOn:boolean){
+        if(isOn){
+            console.log("セキュリティを作動しました");
+        }else{
+            console.log("セキュリティを停止しました");
+        }
     }
 }
 
-
-
-//以下はアクセス不可
-// redHouse.rooms=2;
-// redHouse.countRooms();
-// redHouse.address="千葉県"
-// redHouse.getAddress();
+class GarageHouse extends House{
+    //車庫の有無を表すプロパティ(true:車庫あり/false:車庫なし)
+    public hasGarage:boolean=false;
+    //車庫の開閉をするメソッドの定義(true:車庫を開ける/false:車庫を閉める)
+    public openGarage(isOpen:boolean):void{
+        if(this.hasGarage){ //車庫があるかどうか
+            if(isOpen){ //車庫を開ける場合
+                console.log("車庫を開けました");
+            }else{　//車庫を閉じる場合
+                console.log("車庫を閉じました");
+            }
+        }else{ //車庫が無い場合
+            console.log("車庫がありません")
+        }
+    }
+}
+const gh=new GarageHouse();//インスタンスの作成
+gh.hasGarage=true;          //車庫を「あり」に設定
+gh.openGarage(true);        //車庫を空ける
+gh.openGarage(false);       //車庫を閉める
+gh.activateSecurity(true);  //セキュリティを作動させる
