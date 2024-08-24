bbba88c35b17c6e5940bf56c774a2aca2caa5852
ura-hasam <s.yamane0601@gmail.com>
2024-08-24 15:23:55 +0900

    オーバーライドの学習

--- js/index.js
+++ js/index.js
@@ -14,28 +14,17 @@ class House {
     }
 }
 class GarageHouse extends House {
-    constructor() {
-        super(...arguments);
-        //車庫の有無を表すプロパティ(true:車庫あり/false:車庫なし)
-        this.hasGarage = false;
-    }
-    //車庫の開閉をするメソッドの定義(true:車庫を開ける/false:車庫を閉める)
-    openGarage(isOpen) {
-        if (this.hasGarage) { //車庫があるかどうか
-            if (isOpen) { //車庫を開ける場合
-                console.log("車庫を開けました");
-            }
-            else { //車庫を閉じる場合
-                console.log("車庫を閉じました");
-            }
+    activateSecurity(isOn) {
+        if (isOn) {
+            console.log("セキュリティを作動しました");
+            console.log("ガレージのセキュリティも作動します");
         }
-        else { //車庫が無い場合
-            console.log("車庫がありません");
+        else {
+            console.log("セキュリティを停止しました");
+            console.log("ガレージのセキュリティも停止します");
         }
     }
 }
-const gh = new GarageHouse(); //インスタンスの作成
-gh.hasGarage = true; //車庫を「あり」に設定
-gh.openGarage(true); //車庫を空ける
-gh.openGarage(false); //車庫を閉める
-gh.activateSecurity(true); //セキュリティを作動させる
+const gh = new GarageHouse();
+gh.activateSecurity(true);
+gh.activateSecurity(false);
--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,17 @@ class House{
 }
 
 class GarageHouse extends House{
-    //車庫の有無を表すプロパティ(true:車庫あり/false:車庫なし)
-    public hasGarage:boolean=false;
-    //車庫の開閉をするメソッドの定義(true:車庫を開ける/false:車庫を閉める)
-    public openGarage(isOpen:boolean):void{
-        if(this.hasGarage){ //車庫があるかどうか
-            if(isOpen){ //車庫を開ける場合
-                console.log("車庫を開けました");
-            }else{　//車庫を閉じる場合
-                console.log("車庫を閉じました");
-            }
-        }else{ //車庫が無い場合
-            console.log("車庫がありません")
+    public activateSecurity(isOn: boolean){
+        if(isOn){
+            console.log("セキュリティを作動しました");
+            console.log("ガレージのセキュリティも作動します");
+        }else{
+            console.log("セキュリティを停止しました");
+            console.log("ガレージのセキュリティも停止します");
         }
     }
 }
-const gh=new GarageHouse();//インスタンスの作成
-gh.hasGarage=true;          //車庫を「あり」に設定
-gh.openGarage(true);        //車庫を空ける
-gh.openGarage(false);       //車庫を閉める
-gh.activateSecurity(true);  //セキュリティを作動させる
+
+const gh=new GarageHouse();
+gh.activateSecurity(true);
+gh.activateSecurity(false);
