8f3e033962dfea0b0c0486e0f28cce66806733db
ura-hasam <s.yamane0601@gmail.com>
2024-08-31 17:08:29 +0900

    抽象クラスの学習

--- js/index.js
+++ js/index.js
@@ -1,49 +1,10 @@
 "use strict";
-//復習問題9-1
-class House {
-    constructor() {
-        this.color = "白";
-        this.rooms = 1;
-    }
-    activateSecurity(isOn) {
-        if (isOn) {
-            console.log("セキュリティを作動しました");
-        }
-        else {
-            console.log("セキュリティを停止しました");
-        }
+class Zukei {
+}
+class Triangle extends Zukei {
+    Draw() {
+        console.log("三角形を描画します");
     }
 }
-class FlyingHouse extends House {
-    Fly() {
-        console.log("空を飛びます");
-    }
-}
-const fh = new FlyingHouse;
-// fh.Fly ←自分の解答:()が入っていない
-fh.Fly(); //解答
-//復習問題9-2
-// class Calculator{
-//     add(int:number,int2:number):number;
-//     add(int:number,int2:number,int3?:number){
-//         if(int3){
-//             return int+int2+int3
-//         }else{
-//             return int+int2
-//         }
-//     }
-// }
-// const sum=new Calculator;
-// sum.add(1,2);　以上のコードではエラーが発生し動かなかった。
-//以下正解の解答
-class Calculator {
-    add(x, y, z) {
-        if (z !== undefined) { //zがundefinedが未定義である場合
-            return x + y + z;
-        }
-        return x + y;
-    }
-}
-const calc = new Calculator();
-console.log(calc.add(2, 3));
-console.log(calc.add(2, 3, 4));
+const tri = new Triangle();
+tri.Draw();
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,12 @@
-//復習問題9-1
-class House{
-    public color:string="白";
-    public rooms:number=1;
+abstract class Zukei{
+    abstract Draw():void;
+}
 
-    public activateSecurity(isOn:boolean){
-        if(isOn){
-            console.log("セキュリティを作動しました");
-        }else{
-            console.log("セキュリティを停止しました");
-        }
+class Triangle extends Zukei{
+    Draw(): void {
+        console.log("三角形を描画します");
     }
 }
 
-class FlyingHouse extends House{
-    Fly():void{//フライメソッドの作成
-        console.log("空を飛びます");
-    }
-}
-
-const fh= new FlyingHouse;
-// fh.Fly ←自分の解答:()が入っていない
-fh.Fly();//解答
-
-//復習問題9-2
-// class Calculator{
-//     add(int:number,int2:number):number;
-//     add(int:number,int2:number,int3?:number){
-//         if(int3){
-//             return int+int2+int3
-//         }else{
-//             return int+int2
-//         }
-//     }
-// }
-
-// const sum=new Calculator;
-// sum.add(1,2);　以上のコードではエラーが発生し動かなかった。
-//以下正解の解答
-class Calculator{
-    add(x:number,y:number):number;//x,yを受け取り、
-    add(x:number,y:number,z:number):number;//x,y,zを受け取り、それらを加算してその結果を返すオーバーロード
-    add(x:number,y:number,z?:number):number{//addメソッドの共通定義部分
-        if(z !==undefined){//zがundefinedが未定義である場合
-            return x+y+z;
-        }
-        return x+y;
-    }
-}
-
-const calc=new Calculator();
-console.log(calc.add(2,3,));
-console.log(calc.add(2,3,4));
+const tri =new Triangle();
+tri.Draw();
