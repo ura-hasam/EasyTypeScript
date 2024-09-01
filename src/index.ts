cf3d39ef3a7be35a19edce18f5d98d2ff24c3daa
ura-hasam <s.yamane0601@gmail.com>
2024-09-01 16:24:26 +0900

    Errorオブジェクトについて

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,22 @@ try{
 
     const ans=x/y; //例外が発生するのでここは実行されない
     console.log(ans);
-}catch{
-    console.log("エラーが発生しました");
+}catch(err){
+    console.log(err);
+}finally{
+    console.log("プログラムを終了します");
+}
+
+try {
+    throw new RangeError("範囲外エラー");
+}catch(err){
+    if (err instanceof SyntaxError){
+        console.log("構文エラーが発生しました。");
+    }else if(err instanceof RangeError){
+        console.log("範囲外の操作が行われました");
+    }else{
+        console.log("その他の例外が発生しました");
+    }
 }finally{
     console.log("プログラムを終了します");
 }
