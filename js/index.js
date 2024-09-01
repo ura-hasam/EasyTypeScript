425bc4389caa083b3b42e8b1a6e2bd495f25c568
ura-hasam <s.yamane0601@gmail.com>
2024-09-01 15:43:41 +0900

    例外処理とは

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 "use strict";
-// 復習問題10-1 抽象クラスの作成,10-2
-class Zukei {
+const x = 5;
+const y = 0;
+if (y === 0) {
+    //yが0のときは例外をスローしてプログラムを終了
+    throw new Error("ゼロで割り算はできません");
 }
-class Eclipse extends Zukei {
-    Draw() {
-        console.log("楕円を描画します");
-    }
-}
-const ep = new Eclipse;
-ep.Draw;
+const ans = x / y;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-// 復習問題10-1 抽象クラスの作成,10-2
-abstract class Zukei{
-    abstract Draw():void;
+const x:number=5;
+const y:number=0;
+
+if(y===0){
+    //yが0のときは例外をスローしてプログラムを終了
+    throw new Error("ゼロで割り算はできません");
 }
 
-class Eclipse extends Zukei{
-    Draw(): void {
-        console.log("楕円を描画します");
-    }
-}
-
-const ep= new Eclipse;
-ep.Draw;
-
-//インターフェイスの作成
-interface Telephone{
-    call():void;
-}
+const ans=x/y;
