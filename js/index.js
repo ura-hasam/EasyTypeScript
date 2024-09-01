b081f2dbd78657f7229b5ba472c8a87dda263b88
ura-hasam <s.yamane0601@gmail.com>
2024-09-01 11:03:23 +0900

    インターフェイスの継承

--- js/index.js
+++ js/index.js
@@ -13,3 +13,26 @@ class MusicPlayer {
 const mp = new MusicPlayer();
 mp.play();
 mp.stop();
+class VideoPlayer {
+    constructor() {
+        this.videoName = "Sample Video";
+        this.name = "sample";
+    }
+    playVideo() {
+        console.log(`${this.videoName}を再生しました。`);
+    }
+    stopVideo() {
+        console.log(`${this.videoName}を停止しました`);
+    }
+    play() {
+        console.log(`${this.name}を再生しました。`);
+    }
+    stop() {
+        console.log(`${this.name}を停止しました`);
+    }
+}
+const vp = new VideoPlayer();
+vp.playVideo();
+vp.stopVideo();
+vp.play();
+vp.stop();
--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,33 @@ class MusicPlayer implements PlaySound{
 const mp=new MusicPlayer();
 mp.play();
 mp.stop();
+
+interface PlayVideo extends PlaySound{//インターフェイスの継承
+    videoName:string;
+    playVideo():void;
+    stopVideo():void;
+}
+class VideoPlayer implements PlayVideo{
+    videoName: string="Sample Video";
+
+    playVideo(): void {
+        console.log(`${this.videoName}を再生しました。`);
+    }
+    stopVideo(): void {
+        console.log(`${this.videoName}を停止しました`);
+    }
+
+    name:string="sample";
+    play(): void {
+        console.log(`${this.name}を再生しました。`);
+    }
+    stop(): void {
+        console.log(`${this.name}を停止しました`);
+    }
+}
+
+const vp=new VideoPlayer();
+vp.playVideo();
+vp.stopVideo();
+vp.play();
+vp.stop();
