946845a3605a69c90a5e05fdca36dc026a0e1c05
ura-hasam <s.yamane0601@gmail.com>
2024-09-24 00:36:41 +0900

    12章デフォルトエクスポートの学習

--- js/index.js
+++ js/index.js
@@ -1,35 +1,3 @@
-"use strict";
-//問題11-1,2,3
-try {
-    throw new Error("未知のエラーが発生しました");
-}
-catch (err) {
-    console.log(err);
-}
-finally {
-    console.log("処理が正常に終了しました。");
-}
-//問題11-4
-class MyException extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-try {
-    throw new Error("未知のエラーが発生しました");
-}
-catch (err) {
-    console.log(err);
-}
-//問題11-5
-class FileNotException extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-try {
-    throw new Error("未知のエラーが発生しました");
-}
-catch (err) {
-    console.log(err);
+export default function showName(name) {
+    console.log(`${name}さん`);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,3 @@
-//問題11-1,2,3
-try{
-    throw new Error("未知のエラーが発生しました")
-}catch(err){
-    console.log(err);
-}finally{
-    console.log("処理が正常に終了しました。")
-}
-
-
-//問題11-4
-class MyException extends Error{
-    constructor(message:string){
-        super(message);
-    }
-}
-
-try{
-    throw new Error("未知のエラーが発生しました")
-}catch(err){
-    console.log(err);
-}
-
-//問題11-5
-class FileNotException extends Error{
-    constructor(message:string){
-        super(message);
-    }
-}
-
-try{
-    throw new Error("未知のエラーが発生しました")
-}catch(err){
-    console.log(err);
+export default function showName(name:string){
+    console.log(`${name}さん`)
 }
